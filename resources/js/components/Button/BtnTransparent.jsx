c233e96b0cf5db91135afb23cbae661811b1c1ba
Thomas <totoalexandre6259@gmail.com>
2023-08-14 20:50:55 +0200

    SetUp OnClick

--- resources/js/components/Button/BtnTransparent.jsx
+++ resources/js/components/Button/BtnTransparent.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const BtnTransparent = ({ content = 'no content', className }) => {
+const BtnTransparent = ({ content = 'no content', className, onClick = null }) => {
     return (
-        <button className={`bg-transparent border-4 text-font  border-primary mx-auto h-16 w-80  rounded-2xl shadow-2xl ${className}`}>
+        <button onClick={onClick} className={`bg-transparent border-4 text-font  border-primary mx-auto h-16 w-80  rounded-2xl shadow-2xl ${className}`}>
             {content}
         </button>
     )
