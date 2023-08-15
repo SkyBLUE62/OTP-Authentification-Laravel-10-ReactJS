d3cbea8833177b4dac119db53166f2b550f7448f
Thomas <totoalexandre6259@gmail.com>
2023-08-15 22:35:29 +0200

    test format phone

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import Home from './Home/Home';
 import Footer from './Footer/Footer';
-
+import Test from './test/test';
 const App = () => {
 
     return (
         <div className='bg-primary h-screen w-screen shadow-xl'>
-            <Home />
-            <Footer />
+            {/* <Home />
+            <Footer /> */}
+            <Test />
         </div>
+
     );
 }
 
