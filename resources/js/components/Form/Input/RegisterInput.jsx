528cacf7031e3e50e567ae7dbfc78f75aa8ad481
Thomas <totoalexandre6259@gmail.com>
2023-08-15 19:00:49 +0200

    Update animation

--- resources/js/components/Form/Input/RegisterInput.jsx
+++ resources/js/components/Form/Input/RegisterInput.jsx
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { AiOutlineEye, AiOutlineLock, AiOutlineMail } from 'react-icons/ai';
+import { BsPhone } from 'react-icons/bs';
+const RegisterInput = ({ label, name, type, register, errors, watch, ...props }) => {
 
-const RegisterInput = ({ label, name, type, register, errors, ...props }) => {
+    let validationRules = {};
+    const inputRef = useRef(null);
+    const watchPassword = watch('password', '');
+    switch (name) {
+        case 'username':
+            validationRules = {
+                required: `The field ${label} is required.`,
+            };
+            break;
+        case 'email':
+            validationRules = {
+                required: `The field ${label} is required.`,
+            };
+            break;
+        case 'phone':
+            validationRules = {
+                required: `The field ${label} is required.`,
+                pattern: {
+                    value: /^\d{10}$/,
+                    message: 'Please enter a valid phone number.',
+                },
+            };
+            break;
+        case 'password':
+            validationRules = {
+                required: `The field ${label} is required.`,
+            };
+            break;
+        case 'confirmpass':
+            validationRules = {
+                required: 'The Confirmation Password field is required.',
+                validate: value =>
+                    value === watchPassword || 'The passwords do not match.',
+            };
+            break;
+        default:
+            break;
+    }
+
     return (
-        <div className='flex flex-col border-1.5 rounded-lg border-secondary relative h-14'>
+        <div id={`global_${name}`} className={`flex flex-col border-1.5 rounded-lg border-secondary relative h-14 animate__animated ${props.errorAnimation}`}>
             <label htmlFor={props.id} className='absolute top-0 left-2 transform -translate-y-1/2 bg-card px-1'>
                 {label}
             </label>
-            <div className='flex relative flex-row w-full h-full'>
+            <div className='flex relative flex-row w-full h-full '>
                 <input
                     type={type}
-                    {...register(name)}
+                    {...register(name, validationRules)}
                     className='w-full h-full bg-transparent outline-none ml-3'
                 />
                 <span className='mx-2 text-xl flex items-center justify-center'>
-                    {type === 'text' ? <AiOutlineEye /> : type === 'password' ? <AiOutlineLock /> : type === "email" ? <AiOutlineMail /> : null}
+                    {type === 'text' ? <AiOutlineEye /> : type === 'password' ? <AiOutlineLock /> : type === "email" ? <AiOutlineMail /> : type === "tel" ? <BsPhone /> : null}
                 </span>
+
             </div>
-            {errors[name] && <p>{errors[name].message}</p>}
         </div>
     );
 };
--- resources/js/components/Form/RegisterForm.jsx
+++ resources/js/components/Form/RegisterForm.jsx
@@ -5,14 +5,38 @@ import RegisterInput from './Input/RegisterInput';
 import { useForm } from "react-hook-form"
 
 const RegisterForm = ({ initRegister, status }) => {
-    const [view, setView] = useState(false);
-    const [animation, setAnimation] = useState('');
     const {
         register,
         handleSubmit,
         watch,
-        formState: { errors },
+        clearErrors,
+        reset,
+        formState,
+        formState: { errors, isSubmitSuccessful },
+
     } = useForm()
+    const [view, setView] = useState(false);
+    const [animation, setAnimation] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const [fieldAnimations, setFieldAnimations] = useState({
+        username: errors.username ? 'animate__shakeX' : '',
+        email: errors.email ? 'animate__shakeX' : '',
+        phone: errors.phone ? 'animate__shakeX' : '',
+        password: errors.password ? 'animate__shakeX' : '',
+        confirmpass: errors.confirmpass ? 'animate__shakeX' : '',
+    })
+
+    useEffect(() => {
+        setFieldAnimations(prevFieldAnimations => ({
+            ...prevFieldAnimations,
+            username: errors.username ? 'animate__shakeX' : '',
+            email: errors.email ? 'animate__shakeX' : '',
+            phone: errors.phone ? 'animate__shakeX' : '',
+            password: errors.password ? 'animate__shakeX' : '',
+            confirmpass: errors.confirmpass ? 'animate__shakeX' : '',
+        }));
+    }, [errors]);
 
     useEffect(() => {
         if (status === true) {
@@ -20,7 +44,7 @@ const RegisterForm = ({ initRegister, status }) => {
             setAnimation('animate__animated animate__backInUp');
             document.body.style.overflow = 'hidden';
         }
-    }, [status]);
+    }, [status])
 
     const handleClose = () => {
         setAnimation('animate__animated animate__backOutDown');
@@ -33,6 +57,7 @@ const RegisterForm = ({ initRegister, status }) => {
 
     function onSubmit(data) {
         console.log(data)
+        
     }
 
     return (
@@ -42,22 +67,28 @@ const RegisterForm = ({ initRegister, status }) => {
                     <div className='flex flex-col space-y-6 items-center font-montserrat text-secondary'>
                         <div className='flex flex-col min-w-[90%]'>
                             <span className='items-start self-start mt-5 text-lg'>Hello...</span>
-                            <div className='flex flex-row min-w-full justify-between mb-6'>
+                            <div className='flex flex-row min-w-full justify-between'>
                                 <h1 className='text-3xl font-bold'>Register</h1>
                                 <AiOutlineCloseCircle onClick={handleClose} className='text-danger text-3xl cursor-pointer' />
                             </div>
+                            {errors.username && <span className='text-xs text-danger '>{errors.username.message}</span>}
+                            {errors.email && <span className='text-xs text-danger '>{errors.email.message}</span>}
+                            {errors.phone && <span className='text-xs text-danger '>{errors.phone.message}</span>}
+                            {errors.password && <span className='text-xs text-danger '>{errors.password.message}</span>}
+                            {errors.confirmpass && <span className='text-xs text-danger '>{errors.confirmpass.message}</span>}
+
                         </div>
 
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className='flex flex-col space-y-5'>
-                                <RegisterInput label='Username' id='username' name='username' type='text' register={register} errors={errors} />
-                                <RegisterInput label='Email' id='email' name='email' type='email' register={register} errors={errors} />
-                                <RegisterInput label='Password' id='password' name='password' type='password' register={register} errors={errors} />
-                                <RegisterInput label='Confirm Password' id='confirmpass' name='confirmpass' type='password' register={register} errors={errors} />
-                                <BtnForm type='submit'>Submit</BtnForm>
+                                <RegisterInput errorAnimation={fieldAnimations.username} label='Username' id='username' name='username' type='text' register={register} errors={errors} watch={watch} />
+                                <RegisterInput errorAnimation={fieldAnimations.email} label='Email' id='email' name='email' type='email' register={register} errors={errors} watch={watch} />
+                                <RegisterInput errorAnimation={fieldAnimations.phone} label='Phone' id='phone' name='phone' type='tel' register={register} errors={errors} watch={watch} />
+                                <RegisterInput errorAnimation={fieldAnimations.password} label='Password' id='password' name='password' type='password' register={register} errors={errors} watch={watch} />
+                                <RegisterInput errorAnimation={fieldAnimations.confirmpass} label='Confirm Password' id='confirmpass' name='confirmpass' type='password' register={register} errors={errors} watch={watch} />
+                                <BtnForm type='submit'></BtnForm>
                             </div>
                         </form>
-
                         <span className='text-base'>
                             Already have an account? <button className='text-danger font-semibold'>Login</button>
                         </span>
