3f07f552e13fa43f5575f70d57db7c99528ffcd6
Thomas <totoalexandre6259@gmail.com>
2023-08-19 22:20:27 +0200

    add logout btn

--- resources/js/components/Home/Home.jsx
+++ resources/js/components/Home/Home.jsx
@@ -11,41 +11,75 @@ import AuthCheck from '../utils/AuthCheck'
 
 
 const Home = () => {
+    const [viewRendu, setViewRendu] = useState(false)
     const [viewRegister, setViewRegister] = useState(false);
     const [viewLogin, setViewLogin] = useState(false);
-    const location = useLocation();
+    const [viewLogout, setViewLogout] = useState(false);
     const errorMessage = location.state?.errorMessage;
     const [user, setUser] = useState(false)
 
     const initRegister = () => {
-        setViewRegister(!viewRegister)
-    }
+        setViewRegister(!viewRegister);
+    };
+
     const initLogin = () => {
-        setViewLogin(!viewLogin)
+        setViewLogin(!viewLogin);
+    };
+
+    const initLogout = async () => {
+        try {
+            const response = await axios.get('/api/logout');
+            if (response.status === 200) {
+                setUser(false)
+                setViewLogout(false)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     useEffect(() => {
         async function fetchData() {
-            const isAuthenticated = await AuthCheck();
-            setUser(isAuthenticated);
+            try {
+                const isAuthenticated = await AuthCheck();
+                console.log(isAuthenticated)
+                if (isAuthenticated !== false) {
+                    setUser(isAuthenticated);
+                    setViewLogout(true)
+                    setViewRendu(true)
+                } else {
+                    setViewRendu(true)
+                    setUser(isAuthenticated);
+                }
+            } catch (error) {
+                setViewLogout(false)
+            }
         }
         fetchData();
     }, []);
 
     return (
-        <>
-            <MainTitle content='Welcome' user={user} animation='animate__animated animate__fadeInUp' />
-            <HomeDescription animation="animate__animated animate__fadeInUp" />
+        viewRendu && (
+            <>
+                <MainTitle content='Welcome' user={user} animation='animate__animated animate__fadeInUp' />
+                <HomeDescription animation="animate__animated animate__fadeInUp" />
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
 
-            {errorMessage && <div className="error-message">{errorMessage}</div>}
+                <div className='flex flex-col mt-10 space-y-6 animate__animated animate__fadeInUp'>
+                    {!viewLogout ? (
+                        <>
+                            <BtnPrimary content='Create Account' onClick={initRegister} />
+                            <BtnTransparent content='Login' onClick={initLogin} />
+                        </>
+                    ) : <BtnPrimary content='Logout' onClick={initLogout} className={'animate__animated animate__fadeInUp'} />}
 
-            <div className='flex flex-col mt-10 space-y-6 animate__animated animate__fadeInUp'>
-                <BtnPrimary content='Create Account' onClick={initRegister} />
-                <BtnTransparent content='Login' onClick={initLogin} />
-            </div>
-            {viewRegister && <RegisterForm status={viewRegister} initRegister={initRegister} />}
-            {viewLogin && <LoginForm status={viewLogin} initLogin={initLogin} />}
-        </>
+                </div>
+                {viewRegister && <RegisterForm status={viewRegister} initRegister={initRegister} />}
+                {viewLogin && <LoginForm status={viewLogin} initLogin={initLogin} />}
+
+            </>
+        )
     )
 }
 
