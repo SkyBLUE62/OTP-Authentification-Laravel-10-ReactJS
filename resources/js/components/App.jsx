e375ff7812ca56da50a40dc2d205a2f6dea3f8b3
Thomas <totoalexandre6259@gmail.com>
2023-08-20 22:00:04 +0200

    add Route forgot password

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -3,13 +3,14 @@ import Home from './Home/Home';
 import PhoneValidation from './OtpValidation/PhoneValidation';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Template from './Template/Template';
-
+import ResetPasswordForm from './Form/ResetPasswordForm';
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Template><Home /></Template>} />
                 <Route path="/phone-validation/:token" element={<Template><PhoneValidation /></Template>} />
+                <Route path="/reset-password/:token" element={<Template><ResetPasswordForm /></Template>} />
             </Routes>
         </Router>
     );
