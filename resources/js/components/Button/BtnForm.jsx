cc84eb4f694a839b09fd632555c576f9cd4af8b7
Thomas <totoalexandre6259@gmail.com>
2023-08-20 14:43:22 +0200

    Add content

--- resources/js/components/Button/BtnForm.jsx
+++ resources/js/components/Button/BtnForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
-const BtnForm = ({ animation }) => {
+const BtnForm = ({ animation, content }) => {
 
     return (
         <button
             type='submit'
             className={` ${animation} animate__animated bg-secondary text-primary font-montserrat text-xl mx-auto font-semibold h-16 w-80 rounded-2xl shadow-2xl`}>
-            Register
+            {content}
         </button>
     );
 }
