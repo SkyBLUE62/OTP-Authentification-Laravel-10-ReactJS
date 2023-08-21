d720da894c062ab68b6e3067772d7c217ce2b3fa
Thomas <totoalexandre6259@gmail.com>
2023-08-21 22:06:06 +0200

    Security route

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Home from './Home/Home';
 import PhoneValidation from './OtpValidation/PhoneValidation';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Template from './Template/Template';
 import ResetPasswordForm from './Form/ResetPasswordForm';
 const App = () => {
@@ -11,6 +11,9 @@ const App = () => {
                 <Route path="/" element={<Template><Home /></Template>} />
                 <Route path="/phone-validation/:token" element={<Template><PhoneValidation /></Template>} />
                 <Route path="/reset-password/:token" element={<Template><ResetPasswordForm /></Template>} />
+
+                <Route path="*" element={<Navigate to="/" />} />
+
             </Routes>
         </Router>
     );
